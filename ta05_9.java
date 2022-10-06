b16531005e58ea5d494e5d1371e385c3a325b6ec
fcarri <fcs0000@gmail.com>
2022-10-06 19:24:18 +0200

    Commit 17

---
+++ ta05_9.java
@@ -0,0 +1,17 @@
+package project5;
+
+public class ta05_9 {
+	public static void main(String args[]){
+		
+		for (int a = 1;a <=100;a++) {			
+			
+			if (a % 2 == 0) {
+				System.out.println("El nombre "+a+" és divisible per 2");
+			}else if (a % 3 == 0) {
+				System.out.println("El nombre "+a+" és divisible per 3");
+			}
+			
+		}
+		
+	}
+}
